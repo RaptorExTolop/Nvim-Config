195f5c1c9e74c80138b1a1791c8f6151b62323ab
raptorextolop <jamesappletonret@gmail.com>
2026-03-26 22:19:44 +1030

    removed background colours so I can have transparent background

--- after/plugin/colours.lua
+++ after/plugin/colours.lua
@@ -2,8 +2,8 @@ function colourMyPencils(colour)
 	colour = colour or "kanagawa-wave"
 	vim.cmd.colorscheme(colour)
 
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 colourMyPencils()
