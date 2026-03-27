3ed147e6c6cce2502d9e83152e6ac7005bc3d3fc
raptorextolop <jamesappletonret@gmail.com>
2026-03-27 18:11:57 +1030

    WE GOT RID OF THE THINGY THAT HAPPENS TO BE CALLED FUGITIVE I TOTALLY DIDN'T FORGET THAT WAIT WHAT WAS I SAYINg oh fugative was fucking with my TRANSPERANCY or however you spell that fucking word I hate english oh wait I swear wait I need to change wait no don't end this messa

--- after/plugin/colours.lua
+++ after/plugin/colours.lua
@@ -4,6 +4,16 @@ function colourMyPencils(colour)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+	vim.api.nvim_set_hl(0, "GitSignsAdd", { bg = "none" })
+	vim.api.nvim_set_hl(0, "GitSignsChange", { bg = "none" })
+	vim.api.nvim_set_hl(0, "GitSignsDelete", { bg = "none" })
+	vim.api.nvim_set_hl(0, "GitSignsAddNr", { bg = "none" })
+	vim.api.nvim_set_hl(0, "GitSignsChangeNr", { bg = "none" })
+	vim.api.nvim_set_hl(0, "GitSignsDeleteNr", { bg = "none" })
+	
 end
 
 colourMyPencils()
--- lua/raptorextolop/plugins/kanagawa.lua
+++ lua/raptorextolop/plugins/kanagawa.lua
@@ -2,7 +2,9 @@ return {
 	"rebelot/kanagawa.nvim",
 	name = "kanagawa",
 	priority = 1000,
-	config = function()
-		vim.cmd('colorscheme kanagawa-wave')
-	end
+	opt = {
+		styles = {
+			sidebars = "transparent"
+		}
+	}
 }
